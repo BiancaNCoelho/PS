810f74b61126eb29ae3cede99bdf11bfb6062cd5
Joao Antonio Soares <joaoantoniosoares@Macbookairjoao.local>
2022-05-08 17:23:16 -0300

    Primeira versao da maquina virtual, loads funcionando em todos os formatos de enderecamento testados

---
+++ Memoria.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+public class Memoria {
+    
+    public final byte[] memoria;
+
+    //capacidade == 1KB por exmeplo == 1k entradas na memoria
+    public Memoria(int capacidade){
+        this.memoria = new byte[capacidade];
+    }
+
+    //Zera todas as posicoes da memoria
+    public void reset(){
+
+        Arrays.fill(memoria,(byte)0);
+    }
+
+    public int getByte(int addr){
+        return ((int)memoria[addr]) & 0xFF;
+    }
+    public void setByte(int addr, int value){
+        memoria[addr] = (byte)(value & 0xFF);
+    }
+
+    public void setword(int addr, int value){
+        //Coloca o byte mais significativo em addr e menos signf no addr+2
+        setByte(addr, value >> 16);
+        setByte(addr + 1, value >> 8);
+        setByte(addr + 2, value);
+    }
+    public int getWord(int addr){
+        int MSB = getByte(addr) <<16;
+        int MID = getByte(addr + 1) <<8;
+        int LSB = getByte(addr + 2);
+        // Ex: 0xE10000 + 0xE100 + 0xE1 = 0xE1E1E1
+        return MSB + MID + LSB;
+    }
+
+}
---
+++ Testes.java
@@ -0,0 +1,63 @@
+public class Testes {
+    public static void main(String[] args) {
+        Maquina xp = new Maquina();
+        Memoria ram = xp.memory;
+        Registers regis = xp.registers;
+
+        /*
+            INSTRUÇÕES LOAD DA FIGURA 1.1 NA PÁGINA 11 DO LIVRO B.LELAND
+        */
+        //TABELA DE ENDEREÇOS DA FIGURA:
+        ram.setword(0x3030,0x003600);
+        ram.setword(0x3600, 0x103000);
+        ram.setword(0x6390,0x00C303);
+        ram.setword(0xC303,0x003030);
+        // 1) Obj code = 0x032600
+        ram.setword(0x2FFD, 0x032600);
+        regis.setRegValue(0x2FFD, 6); // (PC) endereço 1ª da instrução
+        xp.exec();
+        System.out.println("1) Conteúdo registradores:");
+        regis.show_registers();
+        regis.resetRegisters();
+        //2) Obj code = 0x3C300
+        ram.setword(0x50, 0x3C300);
+        regis.setRegValue(0x50,6);
+        regis.setRegValue(0x006000, 3); // (B) == 006000
+        regis.setRegValue(0x000090, 1); // (X) == 000090
+        xp.exec();
+        System.out.println("2) Conteúdo registradores:");
+        regis.show_registers();
+        regis.resetRegisters();
+        // 3) Obj code = 0x22030
+        ram.setword(0x2FFD, 0x22030);
+        regis.setRegValue(0x2FFD, 6);
+        xp.exec();
+        System.out.println("3) Conteúdo registradores:");
+        regis.show_registers();
+        regis.resetRegisters();
+        // 4) Obj code = 0x010030
+        ram.setword(0x0, 0x010030);
+        regis.setRegValue(0, 6); // PC
+        xp.exec();
+        System.out.println("4) Conteúdo registradores:");
+        regis.show_registers();
+        regis.resetRegisters();
+        // 5) Obj code = 0x003600
+        ram.setword(0x2FFD, 0x003600);
+        regis.setRegValue(0x2FFD, 6); //PC
+        xp.exec();
+        System.out.println("5) Conteúdo registradores:");
+        regis.show_registers();
+        regis.resetRegisters();
+        // 6) Obj code = 0x0310C303
+        ram.setword(0x100,0x0310C3);
+        ram.setByte(0x103, 0x03);
+        regis.setRegValue(0x100, 6);
+        xp.exec();
+        System.out.println("6) Conteúdo registradores:");
+        regis.show_registers();
+        regis.resetRegisters();
+
+    }
+
+}
